da0881062b3fb2ecb72247343dce39a0ea39a345
Frankline Were <franklinegift@gmail.com>
2024-08-01 23:23:34 +0300

    add jsx

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,14 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+  <div id="about">
+    <h2>About Me</h2>
+    <p>I have a deep passion for creating wonderful experiences for users of the web.
+      I spend a better part of my day crafting user-friendly and visually appealing websites.
+    </p>
+    <img src={image} alt="I made this" />
+    </div>);
 }
 
 export default About;
